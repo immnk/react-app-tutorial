bb52f4edacfe998ea8cf7cb6a7c4921dd59a7d01
Manikanta Tankala <im.manikanta@gmail.com>
2019-05-19 17:24:54 +0800

    scss changes and card list git example

---
+++ src/Lesson3/Lesson3.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import './Lesson3.scss';
+
+const testData = [
+    {name: "Dan Abramov", avatar_url: "https://avatars0.githubusercontent.com/u/810438?v=4", company: "@facebook"},
+    {name: "Sophie Alpert", avatar_url: "https://avatars2.githubusercontent.com/u/6820?v=4", company: "Humu"},
+    {name: "Sebastian Markbåge", avatar_url: "https://avatars2.githubusercontent.com/u/63648?v=4", company: "Facebook"},
+];
+
+class CardList extends React.Component {
+    render() {
+        return (
+            <div>
+                <Card {...testData[0]}/>
+                <Card {...testData[1]}/>
+            </div>
+        );
+    }
+}
+
+class Card extends React.Component {
+    render() {
+        const profile = this.props;
+        return (
+            <div className="github-profile">
+                <img src={profile.avatar_url}/>
+                <div className="info">
+                    <div className="name">{profile.name}</div>
+                    <div className="company">{profile.company}</div>
+                </div>
+            </div>
+        );
+    }
+}
+
+class Lesson3 extends React.Component {
+    render() {
+        return(
+            <div>
+                <h2 className="header">{this.props.title}</h2>
+                <CardList />
+            </div>       
+        );
+    }
+}
+
+export default Lesson3;
