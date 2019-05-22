fa20a20c9ee97523e9998427b641fdc0552ca2d7
Manikanta Tankala <im.manikanta@gmail.com>
2019-05-22 20:33:09 +0530

    Lesson 4- Set up

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Lesson1 from './Lesson1/Lesson1';
 import Lesson2 from './Lesson2/Lesson2';
 import Lesson3 from './Lesson3/Lesson3';
 import Lesson31 from './Lesson3.1/Lesson3.1';
+import Lesson4 from './Lesson4/Lesson4';
 import './App.css';
 
 function App() {
@@ -16,12 +17,14 @@ function App() {
         <li><button className="button" onClick={()=> changeLesson(1)}>Lesson 1</button></li>
         <li><button className="button" onClick={()=> changeLesson(2)}>Lesson 2</button></li>
         <li><button className="button" onClick={()=> changeLesson(3)}>Lesson 3</button></li>
-        <li><button className="button" onClick={()=> changeLesson(4)}>Lesson 3.1</button></li>
+        <li><button className="button" onClick={()=> changeLesson(3.1)}>Lesson 3.1</button></li>
+        <li><button className="button" onClick={()=> changeLesson(4)}>Lesson 4</button></li>
       </ul>
       { lessonNumber === 1 ? <Lesson1 /> : null }
       { lessonNumber === 2 ? <Lesson2 precision={7}/> : null }
       { lessonNumber === 3 ? <Lesson3 title="Welcome to GitHub Cards App"/> : null }
-      { lessonNumber === 4 ? <Lesson31 title="Welcome to GitHub Cards App with functions"/> : null }
+      { lessonNumber === 3.1 ? <Lesson31 title="Welcome to GitHub Cards App with functions"/> : null }
+      { lessonNumber === 4 ? <Lesson4 title="Cards App"/> : null }
     </div>
   );
 }
---
+++ src/Lesson4/Lesson4.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Lesson4 = (props) => {
+    return (
+        <div>
+            <h3>{props.title}</h3>
+        </div>
+    );
+}
+
+export default Lesson4;
