82f24fc5d1e599432b6d406afc77659fdee7afd3
Mani <im.manikanta@gmail.com>
2019-05-31 12:00:57 +0800

    Lesson4: Refactoring

--- src/Lesson4/Lesson4.js
+++ src/Lesson4/Lesson4.js
@@ -1,29 +1,9 @@
 import React, {useState} from 'react';
 import './Lesson4.scss';
-
-const StarsDisplay = props => (
-    <>
-        {utils.range(1, props.count).map(starId => 
-            <div key={starId} className="star" />
-        )}
-    </>
-);
-
-const PlayNumber = props => (
-    <button 
-        className="number" 
-        style={{ backgroundColor: colors[props.status]}}
-        onClick={()=> { props.onClick(props.number, props.status); }}
-    >
-        {props.number}
-    </button>
-);
-
-const PlayAgain = props => (
-    <div className="game-done">
-        <button onClick={props.onClick}>Play Again</button>
-    </div>
-)
+import StarsDisplay from './StarsDisplay';
+import utils from './Util';
+import PlayNumber from './PlayNumber';
+import PlayAgain from './PlayAgain';
 
 const Lesson4 = (props) => {
     // Hooks and side effects
@@ -102,43 +82,5 @@ const Lesson4 = (props) => {
         </div>
     );
 }
-
-// Color Theme
-const colors = {
-    available: 'lightgray',
-    used: 'lightgreen',
-    wrong: 'lightcoral',
-    candidate: 'deepskyblue',
-};
-
-// Math science
-const utils = {
-    // Sum an array
-    sum: arr => arr.reduce((acc, curr) => acc + curr, 0),
-
-    // create an array of numbers between min and max (edges included)
-    range: (min, max) => Array.from({ length: max - min + 1 }, (_, i) => min + i),
-
-    // pick a random number between min and max (edges included)
-    random: (min, max) => min + Math.floor(max * Math.random()),
-
-    // Given an array of numbers and a max...
-    // Pick a random sum (< max) from the set of all available sums in arr
-    randomSumIn: (arr, max) => {
-        const sets = [[]];
-        const sums = [];
-        for (let i = 0; i < arr.length; i++) {
-            for (let j = 0, len = sets.length; j < len; j++) {
-                const candidateSet = sets[j].concat(arr[i]);
-                const candidateSum = utils.sum(candidateSet);
-                if (candidateSum <= max) {
-                    sets.push(candidateSet);
-                    sums.push(candidateSum);
-                }
-            }
-        }
-        return sums[utils.random(0, sums.length)];
-    },
-};
 
 export default Lesson4;
---
+++ src/Lesson4/PlayAgain.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const PlayAgain = props => (
+    <div className="game-done">
+        <button onClick={props.onClick}>Play Again</button>
+    </div>
+);
+
+module.exports = PlayAgain;
+// Same as doing exports.PlayAgain = PlayAgain
+// Same as doing export default PlayAgain
+
+// Imagine this statment present to understand exports
+// exports = module.exports = {}
+// Example
+// exports.function1 = {}
+// exports.function2 = {}
+// is same as 
+// module.exports ={function1: {}, function2: {}}
